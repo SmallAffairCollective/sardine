18956e7394b4f70345e3a043695d18b873badbb1
defermat <defermat@gmail.com>
2014-05-26 14:04:07 -0700

    cleanup, board is working

--- sardine/board.py
+++ sardine/board.py
@@ -1,27 +1,64 @@
 import math
 
 class Board:
-    
     @staticmethod
     def parse(string_board):
         '''
         Converts a string of characters into an array.
         '''
         return [char for char in string_board]
-    
+
     @staticmethod
     def new_empty(size):
         return Board("_"*size*size)
-        
+
     def __init__(self,string_board):
+        # self.board is a listified string
         self.board = Board.parse(string_board)
-        self.size = int(math.sqrt(len(string_board)))
-    
+        self.size = math.sqrt(len(string_board))
+        if self.size != int(self.size):
+            raise "unacceptable board length"
+        else:
+            self.size = int(self.size)
+
     def to_s(self):
         return ''.join(self.board)
-    
+
     def get(self,row,col):
         return self.board[row*self.size + col]
-    
+
+    def get_row(self, row):
+        return self.board[row*self.size:(row*self.size)+self.size]
+
     def set(self,row,col,val):
         self.board[row*self.size + col] = val
+
+    def draw_padding(self, cell_width):
+        return (("|"+(" "*cell_width))*self.size)+"|\n"
+
+    def draw_border(self, cell_width):
+        return (("+"+("-"*cell_width))*self.size)+"+\n"
+
+    def draw_char_row(self, cell_width, row_num):
+        row = "|"
+        row += " "*((cell_width)/2)
+        inbetween_string = " "*((cell_width)/2)+"|"+(" "*((cell_width-1)/2))
+        row += (inbetween_string).join(self.get_row(row_num))
+        row += " "*((cell_width-1)/2)+"|\n"
+        return row
+
+    def draw_row(self, cell_height, cell_width, row_num):
+        row = self.draw_border(cell_width)
+        row += self.draw_padding(cell_width)*((cell_height-1)/2)
+        row += self.draw_char_row(cell_width, row_num)
+        row += self.draw_padding(cell_width)*(cell_height/2)
+        return row
+
+    def drawable(self, grid_width, grid_height):
+        draw_board = ""
+        cell_height = ((grid_height-1)/self.size)-1
+        cell_width = ((grid_width-2)/self.size)-1
+        for row in range(self.size):
+            draw_board += self.draw_row(cell_height, cell_width, row)
+        draw_board += self.draw_border(cell_width)
+        return draw_board
--- sardine/ui.py
+++ sardine/ui.py
@@ -1,10 +1,10 @@
+from board import Board
 import curses
 import math
 import sys
 
-class main(object):
+class UI(object):
     def __init__(self):
-        a = "junk"
         self.win = curses.initscr()
         curses.start_color()
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
@@ -24,40 +24,11 @@ class main(object):
     def display_menu(self, board):
         x = 0
         position = 0
-        board_size = math.sqrt(len(board))
-        if board_size != int(board_size):
-            print "unacceptable board length"
-            curses.endwin()
-        else:
-            board_size = int(board_size)
 
         while x != ord('2'):
-            draw_board = ""
             self.win = curses.initscr()
             win_tuple = self.win.getmaxyx()
-            lines = board_size-1
-            square = ((win_tuple[1]-2)/board_size)-lines
-            padding = (win_tuple[1]-(square*board_size))/2
-
-            j = 0
-            s = " "
-            if square % 2 != 0:
-                s = "  "
-            z = [s]*(((square*board_size)+lines)/2)
-            for i in xrange(((square*board_size)+lines)/2):
-                if i % (square/2) == (((square/2)-1)/2)+1:
-                    z[i] = "x"
-                    if square % 2 != 0:
-                        z[i] = " x"
-                draw_board += (" "*padding)
-                if i % (square/2) == 0:
-                    if j <= board_size:
-                        draw_board += ((("-"*(square-len(z[i])+1))+"+")*lines)+("-"*(square-len(z[i])+1))+"\n"
-                    j += 1
-                elif j <= board_size:
-                    for x in xrange(lines):
-                        draw_board += ((" "*((square/2)-len(z[i])))+z[i]+(" "*(square/2))+"|")
-                    draw_board += (" "*((square/2)-len(z[i])))+z[i]+(" "*(square/2))+"\n"
+            draw_board = board.drawable(win_tuple[1], win_tuple[0]-8)
 
             self.win.clear()
             self.win.addstr(2, 2, "Shall we play a game...", curses.A_STANDOUT)
@@ -69,13 +40,13 @@ class main(object):
             x = self.win.getch()
 
             if x == ord('1'):
-                location = len(board)+1
-                while location > len(board):
+                location = len(board.board)+1
+                while location > len(board.board):
                     location = self.get_param("Enter a location")
                     try:
                         location = int(location)
                     except:
-                        location = len(board)+1
+                        location = len(board.board)+1
                 character = self.get_param("Enter a character")
                 curses.endwin()
             elif x == ord('2'):
@@ -84,7 +55,7 @@ class main(object):
         curses.endwin()
 
 def start():
-    main().display_menu("asdfgasdfgasdfga")
+    UI().display_menu(Board("abcdefghijklmnop"))
 
 if __name__ == "__main__":
     start()
