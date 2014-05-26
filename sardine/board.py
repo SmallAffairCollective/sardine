bfb1ed18d7a6d6bf24aa14460d96f252048027e0
Alex Noguchi <alexander.noguchi@gmail.com>
2014-05-25 23:24:42 -0700

    simple board class

---
+++ sardine/board.py
@@ -0,0 +1,23 @@
+import math
+
+class Board:
+    @staticmethod
+    
+    def parse(string_board):
+        '''
+        Converts a string of characters into an array.
+        '''
+        return [char for char in string_board]
+    
+    def __init__(self,string_board):
+        self.board = Board.parse(string_board)
+        self.size = math.sqrt(len(string_board))
+    
+    def to_s(self):
+        return ''.join(self.board)
+    
+    def get(self,row,col):
+        return self.board[row*self.size + col]
+        
+    def set(self,row,col,val):
+        self.board[row*self.size + col] = val
